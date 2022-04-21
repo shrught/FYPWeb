09f8ab649041291f4cdef52571ff01dfad27a24d
shrught <hans890103@gmail.com>
2022-04-21 11:53:34 +0800

    added pie chart

--- main.py
+++ main.py
@@ -4,6 +4,13 @@
 from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 from __init__ import create_app, db
+import plotly.express as px
+import pandas as pd
+from dash import html as html
+import plotly as py
+import folium
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 ########################################################################################
 # our main blueprint
@@ -21,7 +28,22 @@ def profile():
 @main.route('/reports') 
 @login_required
 def reports():
-    return render_template('reports.html', name=current_user.name)
+    data = pd.read_csv("static/mtgox_accounts.csv", encoding = 'latin1', on_bad_lines='skip')
+    emails = ['gmail', 'yahoo','msn','hotmail','aol', 'org', 'sbcglobal','gmx','mail.ru','nan','edu']
+    pat = '|'.join(emails)
+    s = data['Email'].str.extract('('+ pat + ')', expand=False)
+    df1 = data.groupby(s).size().reset_index(name='Count')
+    df1.loc[6]=['Others', 61006-276-20692-3329-1012-469-1-917-100-3517-144-354]
+    Pie_fig = px.pie(df1, values='Count', names='Email')
+
+
+    return render_template('reports.html', 
+    name=current_user.name,
+    pie_chart_div = Pie_fig.to_html(full_html = False)
+    )
+
+    
+    #pie_chart_div = pieChart.to_html(full_html = False)
 
 app = create_app() # we initialize our flask app using the __init__.py function
 if __name__ == '__main__':
